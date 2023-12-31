0928a6e653847f30e7ac38baa4f1a75f368d1b9d
shikharbahikmagar <shikharbahik5@gmail.com>
2023-12-31 14:13:43 +0545

    blog using appwrite

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./header/header";
+import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./logo";
