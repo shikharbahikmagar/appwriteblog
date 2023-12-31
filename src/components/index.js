f4ea126ac52a46008a71d5e63324894905cfa9b2
shikharbahikmagar <shikharbahik5@gmail.com>
2023-12-31 14:01:58 +0545

    blog using appwrite

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./header/header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/postForm";
+import AuthLayout from './AuthLayout'
+import Input from './Input';
+import Select from './Select';
+import Button from './Button'
+import PostCard from './PostCard'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    AuthLayout,
+    Input,
+    Select,
+    Button,
+    PostCard
+}
